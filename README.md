e4c9c5062ede8959d258941334e65a4fc9fe87e3
朱重迁 <z32664206862022@163.com>
2025-04-09 13:48:35 +0800

    refactor(config): 更新数据库配置以包含新实体
    在数据库配置中添加了 `ThirdPartyLibrary` 和 `ArticleComment` 实体，以支持新的功能模块。同时清理了 README.md 中的重复内容。

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
 bun run build
 ncc build dist/main.js -m -o dist1
 按照 bun install winston nest-winston winston-daily-rotate-file
-
-bun run build
-ncc build dist/main.js -m -o dist1
--- src/config/config.ts
+++ src/config/config.ts
@@ -3,7 +3,8 @@ import { User } from '../apps/user/entities/user.entity';
 import { Tag } from '../apps/tags/entities/tag.entity'
 import { Category } from '../apps/categories/entities/category.entity'; // 确保这个路径正确，并且包含了你的实体文件。
 import { Article } from '../apps/article/entities/article.entity';
-
+import { ThirdPartyLibrary } from '../apps/third-party-library/entities/third-party-library.entity';
+import { ArticleComment } from '../apps/article-comments/entities/article-comment.entity';
 export const databaseConfig = {
     imports: [
         ConfigModule.forRoot({
@@ -19,7 +20,7 @@ export const databaseConfig = {
             username: 'root',
             password: 'zhuzhongqian@123456',
             database: 'test_db',
-            entities: [User, Tag, Category, Article],
+            entities: [User, Tag, Category, Article, ThirdPartyLibrary, ArticleComment],
             synchronize: false
         };
     },
