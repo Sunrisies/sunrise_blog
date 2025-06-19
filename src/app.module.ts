65e6936dfc28f495a2f6e24e3b1153fb1c4e4b2b
朱重迁 <z32664206862022@163.com>
2025-06-19 13:22:26 +0800

    style: 格式化代码以符合 Prettier 配置规范
    统一了代码格式化配置，修改了 `.prettierrc` 文件以适应新的格式化规则：
    - 设置了最大打印宽度为 130 字符。
    - 禁用了行尾分号。
    - 移除了末尾多余的逗号。
    - 设置了引号处理方式为按需使用。
    - 禁用了 JSX 单引号。
    - 设置了换行符为 lf。
    
    移除了 `biome.json` 文件中的配置，因为不再使用 Biome 进行代码格式化和 linting。
    
    更新了 `package.json` 文件，移除了 `@biomejs/biome` 依赖。
    
    格式化了 `src/app.module.ts` 文件的代码风格，包括缩进、引号和括号的使用。

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,58 +1,58 @@
-import { Module } from "@nestjs/common";
-import { APP_INTERCEPTOR } from "@nestjs/core";
-import { ArticleCommentsModule } from "./apps/article-comments/article-comments.module";
-import { ArticleModule } from "./apps/article/article.module";
-import { AuthModule } from "./apps/auth/auth.module";
-import { CategoriesModule } from "./apps/categories/categories.module";
-import { StorageModule } from "./apps/storage/storage.module";
-import { TagsModule } from "./apps/tags/tags.module";
-import { ThirdPartyLibraryModule } from "./apps/third-party-library/third-party-library.module";
-import { ToolsModule } from "./apps/tools/tools.module";
-import { UserModule } from "./apps/user/user.module";
-import { GlobalConfigModule } from "./config/config.module";
-import { MysqlConnectionModule } from "./config/mysql.module";
-import { RedisConnectionModule } from "./config/redis.module";
-import { TransformInterceptor } from "./interceptor/transform.interceptor";
-import { ConfigService } from "@nestjs/config";
+import { Module } from '@nestjs/common'
+import { APP_INTERCEPTOR } from '@nestjs/core'
+import { ArticleCommentsModule } from './apps/article-comments/article-comments.module'
+import { ArticleModule } from './apps/article/article.module'
+import { AuthModule } from './apps/auth/auth.module'
+import { CategoriesModule } from './apps/categories/categories.module'
+import { StorageModule } from './apps/storage/storage.module'
+import { TagsModule } from './apps/tags/tags.module'
+import { ThirdPartyLibraryModule } from './apps/third-party-library/third-party-library.module'
+import { ToolsModule } from './apps/tools/tools.module'
+import { UserModule } from './apps/user/user.module'
+import { GlobalConfigModule } from './config/config.module'
+import { MysqlConnectionModule } from './config/mysql.module'
+import { RedisConnectionModule } from './config/redis.module'
+import { TransformInterceptor } from './interceptor/transform.interceptor'
+import { ConfigService } from '@nestjs/config'
 
-import { VisitLogModule } from "./apps/visit-log/visit-log.module";
-import { MessageModule } from "./apps/message/message.module";
-import { PgConnectionModule } from "./config/postgres.module";
-import { GithubCommitModule } from "./apps/github-commit/github-commit.module";
-import { GithubRepositoriesModule } from "./apps/github-repositories/github-repositories.module";
-import { SitemapModule } from "./apps/sitemap/sitemap.module";
+import { VisitLogModule } from './apps/visit-log/visit-log.module'
+import { MessageModule } from './apps/message/message.module'
+import { PgConnectionModule } from './config/postgres.module'
+import { GithubCommitModule } from './apps/github-commit/github-commit.module'
+import { GithubRepositoriesModule } from './apps/github-repositories/github-repositories.module'
+import { SitemapModule } from './apps/sitemap/sitemap.module'
 @Module({
-	imports: [
-		GlobalConfigModule,
-		MysqlConnectionModule,
-		PgConnectionModule,
-		UserModule,
-		AuthModule,
-		TagsModule,
-		CategoriesModule,
-		ArticleModule,
-		ArticleCommentsModule,
-		ToolsModule,
-		ThirdPartyLibraryModule,
-		StorageModule.forRoot({
-			type: "qiniu",
-			useFactory: (configService: ConfigService) => ({}), // 保留工厂函数结构,
-			inject: [ConfigService],
-		}),
-		RedisConnectionModule,
-		VisitLogModule,
-		MessageModule,
-		GithubCommitModule,
-		GithubRepositoriesModule,
-		SitemapModule,
-	],
-	controllers: [],
-	providers: [
-		{
-			// 全局拦截器
-			provide: APP_INTERCEPTOR,
-			useClass: TransformInterceptor,
-		},
-	],
+  imports: [
+    GlobalConfigModule,
+    MysqlConnectionModule,
+    PgConnectionModule,
+    UserModule,
+    AuthModule,
+    TagsModule,
+    CategoriesModule,
+    ArticleModule,
+    ArticleCommentsModule,
+    ToolsModule,
+    ThirdPartyLibraryModule,
+    StorageModule.forRoot({
+      type: 'qiniu',
+      useFactory: (configService: ConfigService) => ({}), // 保留工厂函数结构,
+      inject: [ConfigService]
+    }),
+    RedisConnectionModule,
+    VisitLogModule,
+    MessageModule,
+    GithubCommitModule,
+    GithubRepositoriesModule,
+    SitemapModule
+  ],
+  controllers: [],
+  providers: [
+    {
+      // 全局拦截器
+      provide: APP_INTERCEPTOR,
+      useClass: TransformInterceptor
+    }
+  ]
 })
-export class AppModule {}
+export class AppModule { }
